79ae4913d5dc4a54d51373b8c60db04988160509
Anirudh R <anirudh4444@gmail.com>
2014-10-17 13:01:45 -0500

    first commit

---
+++ AggieExperts/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AggieExperts::Application.config.secret_token = 'af976d1ad06b5612837e5f165722899c777b86d4bc43ca383ba3122b1efd1880508a7057a80dc3cb0493167d729c4515523dcf78cae515a3f7640e4b71c06940'
