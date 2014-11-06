d0de5fd032ffda77bb6f52bb08f0f786295bfa16
psakuru <prithvi.sakuru@tamu.edu>
2014-11-06 01:12:53 -0600

    moved helper path to application helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-
-  helper_method :admin_path
-  	def admin_path 
-		flash[:alert] = "You have successfully navigated to admin page."
-		'/admin/main'
-	end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+	def admin_path 
+		flash[:alert] = "You have successfully navigated to admin page."
+		'/admin/main'
+	end
 end
