5741906a2dfba6eeb6f0cdc66747500b1c538a50
Anirudh R <anirudh4444@gmail.com>
2014-11-13 22:41:45 -0600

    adding unit tests for search controller

---
+++ spec/controllers/search_controller_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+ 
+describe SearchController do
+  describe 'Checking page' do
+    it 'should call the search controller' do
+        get :search
+        response.should render_template('search')
+    end
+    it 'should read the term parameter from GET' do
+        search_term = 'Antiquity'
+        get :search, {'term' => search_term}
+        assigns(:term).should == search_term
+        response.should render_template('search')
+    end
+    it 'should call the method to find matching profiles in the Profile model' do
+        search_term = 'Antiquity'
+        mocked_profile = double('profile1')
+        mocked_profile.should_receive(:user_id).twice.and_return(1)
+
+        
+        fake_results = [mocked_profile]
+        Profile.should_receive(:get_profiles_by_keyword).with(search_term).and_return(fake_results)
+        get :search, {'term' => search_term}
+    end
+  end
+end
