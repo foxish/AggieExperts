ecd61a78e6681f8308ad49ff71e2be661a132cb3
saisujithreddym <saisujithreddy@gmail.com>
2014-11-23 23:05:20 -0600

    more modifications

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -32,24 +32,34 @@ class Profile < ActiveRecord::Base
   def self.get_profiles_by_name(term)
   @term=term;
   @profiles=[]
-  
+  @profiles2 = []
+  @profiles1 = []
   #@profiles=Profile.where('profiles.name LIKE ?','%'+term+'%').all || []
+   @profiles1=Profile.where('profiles.name LIKE ?','%'+term+'%').all || []
   
   @row1=Status.where('status.code =?', 'ACTIVE').first
-  @row2=User.where('users.status_id=?', @row1.id).first
-  
-  @profiles=Profile.where('profiles.name LIKE ?','%'+term+'%') and Profile.where('profiles.user_id=?',@row2.id ).all || []
+  @row2=User.where('users.status_id=?', @row1.id).all
+ 
+   @row2.each do |r2|  
+    @profiles2+=  Profile.where('profiles.user_id=?',r2.id ) || []
+    end
+    @profiles=@profiles1 & @profiles2
   return @profiles
   end
   
   def self.get_profiles_by_desc(term)
  
   @profiles=[]
-  #@profiles=Profile.where('profiles.description LIKE ?', '%'+ term +'%').all || []
+  @profiles1 = []
+  @profiles2 = []
+  @profiles1=Profile.where('profiles.description LIKE ?', '%'+ term +'%').all || []
    @row1=Status.where('status.code =?', 'ACTIVE').first
-  @row2=User.where('users.status_id=?', @row1.id).first
+  @row2=User.where('users.status_id=?', @row1.id).all
   
-  @profiles=Profile.where('profiles.description LIKE ?','%'+term+'%') and Profile.where('profiles.user_id=?',@row2.id ).all || []
+  @row2.each do |r2|
+  @profiles2+= Profile.where('profiles.user_id=?',r2.id ).all || []
+  end
+  @profiles=@profiles1 & @profiles2
   return @profiles
   end
   
