1b42d326de07e8dd5b66abb003a23c4a48414b7b
psakuru <prithvi.sakuru@tamu.edu>
2014-11-26 14:27:08 -0600

    sign_up page only with valid activation id

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,22 +3,30 @@ skip_before_filter :authorize, only: [:create, :new]
   before_filter :avoid_sign_in, only: [:create, :new], if: :signed_in?
 
   def new
+    session[:id] = nil
     @user = user_from_params
-    render template: 'users/new'
+    if session[:id].nil?
+      redirect_to ("/")
+    else
+      render template: 'users/new'
+    end
   end
 
   def create
     passcheck = validatePassword();
     @user = user_from_params
-    if !passcheck
-      flash[:notice] = "Passwords do not match!!"
-      render template: 'users/new'
-    else
-      if @user.save
-        sign_in @user
-        redirect_back_or url_after_create
+      if !passcheck
+        flash[:notice] = "Passwords do not match!!"
+        render template: 'users/new'
+      else
+        if @user.save
+          Suser.find_by_id(session[:id]).delete
+          session[:id]=@user.id
+          session[:role]=@user.role_id
+          sign_in @user
+          redirect_back_or url_after_create
+        end
       end
-    end
   end
 
   private
@@ -35,15 +43,14 @@ skip_before_filter :authorize, only: [:create, :new]
     email = user_params.delete(:email)
     password = user_params.delete(:password)
     user_params.delete(:re_password)
-    if !params[:aid].nil?
-      session[:email] = Suser.find_by_activation_link(params[:aid]).email
+    if !(params[:aid].nil?) && !(Suser.find_by_activation_link(params[:aid]).nil?)
+      session[:id] = Suser.find_by_activation_link(params[:aid]).id
     end
-    
     Clearance.configuration.user_model.new(user_params).tap do |user|
-      if session[:email].nil? ## bad...need to change this
+      if session[:id].nil?
         user.email = email
       else
-        user.email = session[:email]
+        user.email = Suser.find_by_id(session[:id]).email
       end
       user.password = password
       user.urole_id = User.get_user_role
