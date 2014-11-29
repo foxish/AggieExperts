e3e2d3b71f9d3ffd5b9bb5deb91fe889dc8e956f
Anirudh R <anirudh4444@gmail.com>
2014-11-28 21:10:21 -0600

    Resolved merge hell, removed search/*.js, changed profile.id to profile['id']

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,37 +2,26 @@ class SearchController < ApplicationController
   def search
     @term = params[:term]
     @profiles = []
-    @istermnull=0
+    @istermnull = 0
     
-    if @term==""
-    @istermnull=1
-    
+    if @term == ""
+      @istermnull = 1
     else
       @profiles=Profile.get_profiles_by_keyword(@term)
       @profiles += Profile.get_profiles_by_name(@term)
       @profiles += Profile.get_profiles_by_desc(@term)
-     splitterms=@term.split(" ")
-     splitterms.each do |splitterm|
-      
-      @profiles+=Profile.get_profiles_by_keyword(splitterm)
-      @profiles += Profile.get_profiles_by_name(splitterm)
-      @profiles += Profile.get_profiles_by_desc(splitterm)
-      
+      splitterms = @term.split(" ")
+      splitterms.each do |splitterm|
+        @profiles+=Profile.get_profiles_by_keyword(splitterm)
+        @profiles += Profile.get_profiles_by_name(splitterm)
+        @profiles += Profile.get_profiles_by_desc(splitterm)
       end
       
-      @profiles=@profiles.uniq  
-      
-      @profiles_count=@profiles.count      
+      @profiles=@profiles.uniq
       @keywords = {}
       @profiles.each do |profile|
           @keywords[profile.user_id] = Keyword.get_for_user(profile.user_id)
-            
-         
-    end 
-     
-      
+      end
     end
-    
-       
   end
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -13,20 +13,15 @@ class Profile < ActiveRecord::Base
       @profiles1 = self.joins('JOIN pkeywords ON profiles.user_id = pkeywords.user_id').
                        where('pkeywords.keyword_id =?', @keyword_id ).all || []
 
-       @row1=Status.where('status.code =?', 'ACTIVE').first
-       
-         @row2=User.where('users.status_id=?', @row1.id).all
-         @profiles2 = []
-       @row2.each do |r2|  
-         @profiles2+=Profile.where('profiles.user_id=?',r2.id )|| []
-         
-         end
-      
-       @profiles=@profiles1 & @profiles2
-       
- 
+      @row1 = Status.where('status.code = ?', 'ACTIVE').first
+      @row2 = User.where('users.status_id = ?', @row1['id']).all
+      @profiles2 = []
+      @row2.each do |r2|  
+        @profiles2 += Profile.where('profiles.user_id = ?',r2['id'] ) || []
+      end
+      @profiles = @profiles1 & @profiles2
     end
-    return @profiles
+    return @profiles1
   end
 
   def self.format_phone(number)
@@ -46,43 +41,39 @@ class Profile < ActiveRecord::Base
       end
       j = j+1
     end
-
     return phone_num
   end
   
   def self.get_profiles_by_name(term)
-  @term=term;
-  @profiles=[]
-  @profiles2 = []
-  @profiles1 = []
-  #@profiles=Profile.where('profiles.name LIKE ?','%'+term+'%').all || []
-   @profiles1=Profile.where('profiles.name LIKE ?','%'+term+'%').all || []
-  
-  @row1=Status.where('status.code =?', 'ACTIVE').first
-  @row2=User.where('users.status_id=?', @row1.id).all
+    @term=term;
+    @profiles=[]
+    @profiles2 = []
+    @profiles1 = []
+    
+    @profiles1=Profile.where('profiles.name LIKE ?','%'+ term +'%').all || []
+    @row1=Status.where('status.code = ?', 'ACTIVE').first
+    @row2=User.where('users.status_id = ?', @row1['id']).all
  
-   @row2.each do |r2|  
-    @profiles2+=  Profile.where('profiles.user_id=?',r2.id ) || []
+    @row2.each do |r2|  
+      @profiles2 +=  Profile.where('profiles.user_id = ?', r2['id']) || []
     end
     @profiles=@profiles1 & @profiles2
-  return @profiles
+    return @profiles
   end
   
   def self.get_profiles_by_desc(term)
- 
-  @profiles=[]
-  @profiles1 = []
-  @profiles2 = []
-  @profiles1=Profile.where('profiles.description LIKE ?', '%'+ term +'%').all || []
-   @row1=Status.where('status.code =?', 'ACTIVE').first
-  @row2=User.where('users.status_id=?', @row1.id).all
-  
-  @row2.each do |r2|
-  @profiles2+= Profile.where('profiles.user_id=?',r2.id ).all || []
-  end
-  @profiles=@profiles1 & @profiles2
-  return @profiles
-  end
-  
+    @profiles=[]
+    @profiles1 = []
+    @profiles2 = []
+    @profiles1=Profile.where('profiles.description LIKE ?', '%'+ term +'%').all || []
+    @row1=Status.where('status.code = ?', 'ACTIVE').first
+    @row2=User.where('users.status_id = ?', @row1['id']).all
+
+    @row2.each do |r2|
+      @profiles2+= Profile.where('profiles.user_id=?',r2['id'] ).all || []
+    end
+      @profiles=@profiles1 & @profiles2
+      return @profiles
+    end
 end
 
