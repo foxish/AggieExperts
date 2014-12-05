5d86d5bd438370418f71ccffede8c03e921803a1
Arun Krishnakumar <arunxls@gmail.com>
2014-12-05 15:22:45 -0600

    Added user validation to profile page

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -2,8 +2,7 @@ class ProfileController < ApplicationController
   def show
     @profile = Profile.where(:user_id => params[:id]).first
     if @profile.nil?
-      Profile.create(:user_id => params[:id])
-      redirect_to edit_profile_path(params[:id])
+      redirect_to new_profile_path(params[:id])
     else
       @user = params[:id]
       @publications = Ppublication.where(:user_id => params[:id])
@@ -14,12 +13,26 @@ class ProfileController < ApplicationController
     end
   end
 
+  def new
+    if !current_user.nil? && current_user['id'].to_s == @user
+      Profile.create(:user_id => params[:id])
+      redirect_to edit_profile_path(params[:format])
+    else
+      flash[:notice] = "You do not have permission to do that. Please sign in."
+      redirect_to '/'
+    end
+  end
+
   def edit
     @user = params[:id]
-    
-    @publications = Ppublication.where(:user_id => params[:id])
-    @profile = Profile.where(:user_id => params[:id]).first
-    @keywords = Keyword.get_for_user(params[:id])
+    if !current_user.nil? && (current_user.urole_id == User.get_admin_role || current_user['id'].to_s == @user)
+      @publications = Ppublication.where(:user_id => params[:id])
+      @profile = Profile.where(:user_id => params[:id]).first
+      @keywords = Keyword.get_for_user(params[:id])
+    else
+      flash[:notice] = "You do not have permission to do that. Please sign in."
+      redirect_to profile_path(params[:id])
+    end
   end
 
   def update
@@ -29,7 +42,7 @@ class ProfileController < ApplicationController
     Pkeyword.update({:id => params[:id], :data => params['keyword']})
 
     profile.update_attributes(params['user'])
-    flash[:notice] = " Profile was successfully updated!"
+    flash[:notice] = "Profile was successfully updated!"
     redirect_to profile_path(params[:id])
 
   end
