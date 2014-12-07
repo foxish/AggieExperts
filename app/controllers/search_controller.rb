e0761dd0c30af51632e7e85686fcf79ca239c211
saisujithreddym <saisujithreddy@gmail.com>
2014-12-06 19:18:07 -0600

    some changes

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -12,13 +12,16 @@ class SearchController < ApplicationController
       @profiles += Profile.get_profiles_by_name(@term)
       @profiles += Profile.get_profiles_by_desc(@term)
       splitterms = @term.split(" ")
+      
       splitterms.each do |splitterm|
         @profiles+=Profile.get_profiles_by_keyword(splitterm)
+         
         @profiles += Profile.get_profiles_by_name(splitterm)
+        
         @profiles += Profile.get_profiles_by_desc(splitterm)
-      end
+          end
       
-      @profiles=@profiles.uniq
+      @profiles.uniq!
       @keywords = {}
       @profiles.each do |profile|
           @keywords[profile.user_id] = Keyword.get_for_user(profile.user_id)
