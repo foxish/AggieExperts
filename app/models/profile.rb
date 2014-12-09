8bda4fbf6b04af2ef3974f1e9d08c7903fac87d5
saisujithreddym <saisujithreddy@gmail.com>
2014-12-08 23:42:59 -0600

    profile change to resolve conflict

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,9 +1,33 @@
 class Profile < ActiveRecord::Base
   has_many :pkeywords
   belongs_to :user
-  attr_accessible :user_id, :name, :description, :phone, :email, :title, :website
+
+  attr_accessible :user_id, :name, :description, :phone, :email, :title, :website, :pic
+  has_attached_file :pic, :styles => { :medium => "300x300>", :thumb => "110x100>" }, :default_url => "/assets/default.png"
+  validates_attachment_content_type :pic, :content_type => /\Aimage\/.*\Z/
+
+  validates :name, format: { with: /\s*\w+\s*?\w+?\s*?\w+/,
+     message: "name should be atleast 3 characters long" }
+  validates :description, format: { with: /\s*\w+\s*?\w+?\s*?\w+/,
+     message: "bio should be atleast 3 characters long" }
+
+  validates :phone, format: { with: /^\d{10}$/,
+    message: "Phone number is not of the correct format" }
+
+  validates :website, format: { with: /^$|^http(s?|ftp):\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(\#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/,
+    message: "Website URL is not of the correct format" }
+
+  validates :email, format: { with: /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/,
+    message: "Email is not of the correct format" }
+
+  validates_attachment :pic,
+     :less_than => 1.megabytes, :message => "Photo has to be a jpeg image of size < 1 MB"
+
+
  @profiles = []
+
   def self.get_profiles_by_keyword(term)
+    @row = Keyword.get_match(term)
     @row = Keyword.get_match(term)
     @profiles = []
     @profiles2 = []
@@ -41,8 +65,10 @@ class Profile < ActiveRecord::Base
         end
         j = j+1
       end
+      return phone_num
+    else
+      return ''
     end
-    return phone_num || ""
   end
   
   def self.get_profiles_by_name(term)
@@ -59,16 +85,22 @@ class Profile < ActiveRecord::Base
     return @profiles1
   end
   
+  def remove_pic
+    self.pic = nil;
+  end
+  
   def self.get_profiles_by_desc(term)
    # @profiles=[]
     @profiles1 = []
     @profiles2 = []
     
     @row1=Status.where('status.code = ?', 'ACTIVE').first
+
     @rowid=@row1.id
     @profiles1=self.joins('JOIN users ON profiles.user_id = users.id').where("profiles.description LIKE ?",'%'+ term +'%').where("users.status_id= ?",@rowid).all(:order=> 'profiles.name') || []
     
       return @profiles1
     end
+
 end
 
