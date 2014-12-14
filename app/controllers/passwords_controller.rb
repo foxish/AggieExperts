8bfbeeca28338da00003991abba159c6ff083cdd
DarkCthulhu <anirudh4444@gmail.com>
2014-12-14 14:49:31 -0600

    Removed redundant controllers

--- app/controllers/passwords_controller.rb
+++
@@ -1,4 +0,0 @@
-class PasswordsController < Clearance::PasswordsController
-  skip_before_filter :authorize, only: [:reset_password]
-  before_filter :forbid_non_existent_user, only: [:edit, :update]
-end
