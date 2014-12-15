8f6af0a3cd3300c2983c0223cf785a2643563fa5
DarkCthulhu <anirudh4444@gmail.com>
2014-12-14 22:41:07 -0600

    Added a few more tests

--- spec/models/suser_spec.rb
+++ spec/models/suser_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 describe Suser do
 	describe 'suser model methods' do
 		it 'should insert suser' do
-			SecureRandom.should_receive(:hex).with(25).and_return('123456789012hlgjvhljvjkljkjvgu')
-			Suser.insert_suser('foo@abc.com') == '1234567890'
+			SecureRandom.should_receive(:hex).with(50).and_return('123456789012hlgjvhljvjkljkjvgu')
+			Suser.insert_suser('foo@abc.com', '') == '123456789012hlgjvhljvjkljkjvgu'
 		end
 	end
 end
