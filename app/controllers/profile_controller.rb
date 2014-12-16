40c05123ea1eca5bf8aa261b67c0d4dc6f32ad1c
Arun Krishnakumar <arunxls@gmail.com>
2014-12-15 20:45:21 -0600

    Fixed profile view

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -6,8 +6,7 @@ class ProfileController < ApplicationController
       redirect_to new_profile_path(params[:id])
     else
       if !current_user.nil? && (current_user.urole_id == User.get_admin_role || current_user['id'].to_s == params[:id]) ||
-          !Status.find_by_id(User.find_by_id(params[:id]).status_id).code == 'DISABLE'
-
+          !(Status.find_by_id(User.find_by_id(params[:id]).status_id).code == 'DISABLE')
         @user = params[:id]
         @publications = Ppublication.where(:user_id => params[:id])
         @keywords = Keyword.get_for_user(params[:id])
